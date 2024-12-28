7eff73d46d3c0e71c204ff207c828a7697513511
Sachin Varghese <sachinv2223@gmail.com>
2024-12-28 12:29:33 +0530

    Add logic on adding new conversations (still have doubts)

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import userCircle from "../../assets/user-circle.svg";
 import IndividualMessage from "../../components/IndividualMessage/IndividualMessage";
 import Input from "../../components/Input";
 import { dashboardService } from "../../services/dashboard.service";
-import { iConversation, iMessage } from "../../types/dashboard.types";
+import { iConversation, iMessage, iOtherUser } from "../../types/dashboard.types";
 import { commonService } from "../../services/common.service";
 import { useNavigate } from "react-router-dom";
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
@@ -11,39 +11,6 @@ import { UserPlusIcon } from "@heroicons/react/16/solid";
 
 function Dashboard() {
     const defaultImg = 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-    // const messages: {
-    //     id: string;
-    //     content: string;
-    //     sender: 'self' | 'other';
-    //     timestamp: Date;
-    // }[] = [
-    //         { id: '1', content: 'Hello', sender: 'self', timestamp: new Date() },
-    //         { id: '2', content: 'Hi', sender: 'other', timestamp: new Date() },
-    //         { id: '3', content: 'How are you?', sender: 'self', timestamp: new Date() },
-    //         { id: '4', content: 'I am fine. How about you?', sender: 'other', timestamp: new Date() },
-    //         { id: '5', content: 'I am good. Thank you.', sender: 'self', timestamp: new Date() },
-    //         { id: '6', content: 'You are welcome.', sender: 'other', timestamp: new Date() },
-    //         { id: '7', content: 'I am fine. How about you?', sender: 'self', timestamp: new Date() },
-    //         { id: '8', content: 'I am good. Thank you.', sender: 'other', timestamp: new Date() },
-    //         { id: '9', content: 'You are welcome.', sender: 'self', timestamp: new Date() },
-    //         { id: '10', content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', sender: 'other', timestamp: new Date() },
-    //         { id: '11', content: 'I am good. Thank you.', sender: 'self', timestamp: new Date() },
-    //         { id: '12', content: 'You are welcome.', sender: 'other', timestamp: new Date() },
-    //         { id: '13', content: 'I am fine. How about you?', sender: 'self', timestamp: new Date() },
-    //         { id: '14', content: 'I am good. Thank you.', sender: 'other', timestamp: new Date() },
-    //         { id: '15', content: 'You are welcome.', sender: 'self', timestamp: new Date() },
-    //         { id: '16', content: 'I am fine. How about you?', sender: 'other', timestamp: new Date() },
-    //         { id: '17', content: 'I am good. Thank you.', sender: 'self', timestamp: new Date() },
-    //         { id: '18', content: 'You are welcome.', sender: 'other', timestamp: new Date() },
-    //         { id: '19', content: 'I am fine. How about you?', sender: 'self', timestamp: new Date() },
-    //         { id: '20', content: 'I am good. Thank you.', sender: 'other', timestamp: new Date() },
-    //         { id: '21', content: 'You are welcome.', sender: 'self', timestamp: new Date() },
-    //         { id: '22', content: 'I am fine. How about you?', sender: 'other', timestamp: new Date() },
-    //         { id: '23', content: 'I am good. Thank you.', sender: 'self', timestamp: new Date() },
-    //         { id: '24', content: 'You are welcome.', sender: 'other', timestamp: new Date() },
-    //         { id: '25', content: 'I am fine. How about you?', sender: 'self', timestamp: new Date() },
-    //         { id: '26', content: 'I am the latest one', sender: 'other', timestamp: new Date() },
-    //     ];
     const [isOpenMainChatDropdown, setIsOpenMainChatDropdown] = useState(false);
     const [isOpenUserProfileDropdown, setIsOpenUserProfileDropdown] = useState(false);
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user:data') || ''));
@@ -52,104 +19,8 @@ function Dashboard() {
     const [selectedConversation, setSelectedConversation] = useState(null as iConversation | null);
     const [inputMessage, setInputMessage] = useState('');
     const [openModal, setOpenModal] = useState(false);
-
-    const usersList: {
-        id: string;
-        fullName: string;
-        email: string;
-        img: string
-    }[] = [
-            {
-                id: '1',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '2',
-                fullName: 'Manesh Jeff',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '3',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '4',
-                fullName: 'Manesh Jeff',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '5',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '6',
-                fullName: 'Manesh Jeff',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '7',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '8',
-                fullName: 'Manesh Jeff',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '9',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '10',
-                fullName: 'Manesh Jeff',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '11',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '12',
-                fullName: 'Manesh Jeff',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '13',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '14',
-                fullName: 'Manesh Jeff',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-            {
-                id: '15',
-                fullName: 'John Doe',
-                email: 'l2H3o@example.com',
-                img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            },
-        ]
+    const [conversationCreated, setConversationCreated] = useState(0);
+    const [allUserList, setAllUserList] = useState([] as iOtherUser[]);
 
     const getConversations: () => Promise<iConversation[]> = async () => {
         return user ? await dashboardService.fetchConversations(user?.id, navigate) : [] as iConversation[];
@@ -172,13 +43,73 @@ function Dashboard() {
         }
         console.log('sendInputMessage => ', JSON.stringify(result));
         setInputMessage('');
-        selectedConversation && fetchMessages(selectedConversation);
+        selectedConversation && await fetchMessages(selectedConversation);
     }
 
     const onLogout = async () => {
         dashboardService.logout(navigate);
     }
 
+    const onClickMessageNewPeople = async () => {
+
+        // * get all users and update in allUserList
+        // TODO its not a got way to call this api everytime on clicking MessageNewPeople button if the backend data is not changing
+        const [error, users] = await commonService.catchError(dashboardService.fetchAllUsers(user?.id, navigate));
+        if (error) {
+            console.log(JSON.stringify(`Inside Dashboard:onClickMessageNewPeople ${JSON.stringify(error)}`));
+        }
+        setAllUserList(users as iOtherUser[]);
+
+        // * to open this modal dialog
+        setOpenModal(true);
+    }
+
+    const handleSelectUser = async (receiverData: iOtherUser) => {
+        const [error, result] = await commonService.catchError(dashboardService.createConversation(user?.id, receiverData.id, navigate));
+        if (error) {
+            console.log(JSON.stringify(`Inside Dashboard:CreateConversationError ${JSON.stringify(error)}`));
+        }
+        console.log(`createConversation result => ${JSON.stringify(result)}`);
+
+        // * to update the conversation list
+        // const [errorConv, convs] = await commonService.catchError(getConversations());
+        // if (errorConv) {
+        //     console.log(JSON.stringify(`Inside Dashboard:convError ${JSON.stringify(error)}`));
+        // }
+        // setConversations(convs);
+
+        // ! to trigger useEffect for fetching conversations (can cause issues)
+        setConversationCreated(conversationCreated + 1);
+
+        // * to switch the main chat to the newly created conversation
+        await fetchMessages({
+            conversationId: result.conversationId,
+            otherUser: {
+                id: receiverData.id,
+                fullName: receiverData.fullName,
+                email: receiverData.email
+            }
+        })
+
+        // * close this modal dialog
+        setOpenModal(false);
+    }
+
+    // * to fetch conversation details whenever user changes
+    useEffect(() => {
+        const fetchConversations = async () => {
+            const [error, convs] = await commonService.catchError(getConversations());
+            if (error) {
+                console.log(JSON.stringify(`Inside Dashboard:convError ${JSON.stringify(error)}`));
+            }
+            console.log(JSON.stringify(convs));
+            setConversations(convs);
+        };
+        fetchConversations(); // Call the async function
+        // Optionally, you can add a cleanup function if needed
+    }, [user, conversationCreated]); // Add user as a dependency if you want to refetch when user changes
+
+    // * to close dropdown on outside click
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             const target = event.target as HTMLElement;
@@ -192,20 +123,6 @@ function Dashboard() {
         document.addEventListener('mousedown', handleClickOutside);
         return () => document.removeEventListener('mousedown', handleClickOutside);
     }, [isOpenMainChatDropdown, isOpenUserProfileDropdown]);
-
-    // to fetch conversation details whenever user changes
-    useEffect(() => {
-        const fetchConversations = async () => {
-            const [error, convs] = await commonService.catchError(getConversations());
-            if (error) {
-                console.log(JSON.stringify(`Inside Dashboard:convError ${JSON.stringify(error)}`));
-            }
-            console.log(JSON.stringify(convs));
-            setConversations(convs);
-        };
-        fetchConversations(); // Call the async function
-        // Optionally, you can add a cleanup function if needed
-    }, [user]); // Add user as a dependency if you want to refetch when user changes
 
     return (
         <div className="w-screen h-screen flex flex-row">
@@ -254,7 +171,7 @@ function Dashboard() {
                         <span className="text-lg font-semibold text-center">Messages</span>
 
                         {/* Message new people */}
-                        <div className="p-2 hover:bg-gray-300 rounded-full cursor-pointer transition-colors" onClick={() => setOpenModal(true)}>
+                        <div className="p-2 hover:bg-gray-300 rounded-full cursor-pointer transition-colors" onClick={() => onClickMessageNewPeople()}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                             </svg>
@@ -271,7 +188,7 @@ function Dashboard() {
                                         onClick={() => fetchMessages(conversation)}
                                         key={conversation.conversationId}
                                         id={conversation.conversationId}
-                                        className={`flex items-center py-4 px-2 gap-4 cursor-pointer hover:bg-gray-300 hover:rounded-lg ${index < conversations.length - 1 ? 'border-b border-gray-400' : ''}`}
+                                        className={`flex items-center py-3 px-2 gap-4 cursor-pointer hover:bg-gray-300 hover:rounded-lg ${index < conversations.length - 1 ? 'border-b border-gray-400' : ''}`}
                                     >
                                         <img
                                             src={defaultImg}
@@ -444,7 +361,7 @@ function Dashboard() {
                     <div className="flex min-h-full items-center justify-center p-4 text-center">
                         <DialogPanel
                             transition
-                            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in w-6/12"
+                            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in w-10/12 md:w-8/12 lg:w-6/12"
                         >
                             <div className="bg-white p-8 h-[80vh] flex flex-col min-h-0">
                                 <DialogTitle as="h2" className="flex flex-col">
@@ -453,13 +370,13 @@ function Dashboard() {
                                 </DialogTitle>
                                 <div className="my-4 flex-1 overflow-y-auto">
                                     {
-                                        usersList && usersList.length > 0
+                                        allUserList && allUserList.length > 0
                                             ? <div className="flex flex-col gap-4">
                                                 {
-                                                    usersList.map((user: any) => (
-                                                        <div className="flex items-center gap-4" key={user.id}>
+                                                    allUserList.map((user: iOtherUser) => (
+                                                        <div className="flex items-center gap-4 hover:bg-gray-100 p-2 cursor-pointer rounded-md" key={user.id} onClick={() => handleSelectUser(user)}>
                                                             <img
-                                                                src={user.img}
+                                                                src={'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
                                                                 alt={user.fullName}
                                                                 className="w-10 h-10 rounded-full"
                                                             />
@@ -476,7 +393,7 @@ function Dashboard() {
                                 <div className="flex flex-row-reverse">
                                     <button
                                         type="button"
-                                        // onClick={() => setOpen(false)}
+                                        onClick={() => setOpenModal(false)}
                                         className="inline-flex justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 ml-3 w-auto"
                                     >
                                         Cancel
--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { commonService } from './common.service';
 import api from './api';
-import { iConversation, iMessage } from '../types/dashboard.types';
+import { iConversation, iMessage, iOtherUser } from '../types/dashboard.types';
 import { NavigateFunction } from 'react-router-dom';
 import { authService } from './auth.service';
 
@@ -49,5 +49,31 @@ export const dashboardService = {
     logout: async (navigate: NavigateFunction) => {
         authService.logout();
         navigate('/user/sign_in');
-    }
+    },
+
+    createConversation: async (userId: string, receiverId: string, navigate: NavigateFunction) => {
+        const [error, response] = await commonService.catchError(api.post<any>(`/protected/conversation`, { senderId: userId, receiverId: receiverId }));
+        if (error) {
+            console.error('createConversation ERROR:', JSON.stringify(error));
+            if (['INVALID_REFRESH_TOKEN'].includes(error)) {
+                navigate('/user/sign_in');
+            }
+
+            throw new Error('createConversation ERROR. Please try again.');
+        }
+        return response?.data;
+    },
+
+    fetchAllUsers: async (userId: string, navigate: NavigateFunction) => {
+        const [error, response] = await commonService.catchError(api.get<any>(`/protected/users/${userId}`));
+        if (error) {
+            console.error('fetchAllUsers ERROR:', JSON.stringify(error));
+            if (['INVALID_REFRESH_TOKEN'].includes(error)) {
+                navigate('/user/sign_in');
+            }
+
+            throw new Error('fetchAllUsers ERROR. Please try again.');
+        }
+        return response.data as iOtherUser[];
+    },
 };
