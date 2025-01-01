01344a2e831e70f8e5c3537858cfb1d74c4bdec1
Sachin Varghese <sachinv2223@gmail.com>
2025-01-01 22:39:58 +0530

    solving socket port issue

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -114,7 +114,7 @@ function Dashboard() {
 
     // * to initialize socket
     useEffect(() => {
-        setSocket(() => io(import.meta.env.SOCKET_URL || 'http://localhost:8080'));
+        setSocket(() => io(import.meta.env.SOCKET_URL || 'http://localhost:3000'));
         // console.log(`==> socket useEffect triggerred`);
     }, []);
 
